225f1086392c1ebeb842c3db82698266a030b65f
Calil Drissi <calil.drissi@gmail.com>
2022-05-13 19:54:28 +0000

    route setup

---
+++ routes/auth.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const router = express.Router();
+
+
+// * @route   GET  api/auth
+// * @desc     Get logged in user
+// * @access  Public
+router.post('/', (req, res) => {
+    res.send('register a user endpoint')
+});
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-const  express = require('express');
+const express = require("express");
 
 const app = express();
 
 
-app.get('/', (req, res) =>  res.send('app is on'));
+// Defining endpoints
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
 
+app.get("/", (req, res) => res.json({ msg: "App is on" }));
 
-const PORT = process.env.PORT || '5000';
+const PORT = process.env.PORT || "5000";
 
-app.listen(PORT,  ()=> console.log(`server running on port : ${PORT}`));
+app.listen(PORT, () => console.log(`server running on port : ${PORT}`));
